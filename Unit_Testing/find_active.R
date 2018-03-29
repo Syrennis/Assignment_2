908b2d52c8c95c48cf8e0a64ae83b19b2effee32
Syrennis <bchrisler@ku.edu>
2018-03-29 15:54:17 -0500

    Testing and functions detailed in comments

--- Unit_Testing/find_active.R
+++ Unit_Testing/find_active.R
@@ -6,25 +6,9 @@
 #min      A numeric that is of a certain value and all values above will return
 #
 #Output
-#A vector with the the dates and indicies of all matches
+#A vector with active MJO events
 #
 #Note: no error checking
 #
 
-find_active<-function(ind, min)
-
-
- {  
-  dat<-c()
-
-  for(i in seq_along(ind))
-    {
-      if(ind >= min)
-    {
-      dat<-c(dat,ind)
-    }
-  }
-  
-  return(dat)
-  }
-
+find_active <- function(ind, min)
