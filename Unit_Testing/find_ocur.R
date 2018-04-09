a5e0b87ed56c23ae13407cc91e3fe5570f5dc685
Syrennis <bchrisler@ku.edu>
2018-04-09 17:27:54 -0500

    updated to add second function working with tests

--- Unit_Testing/find_ocur.R
+++ Unit_Testing/find_ocur.R
@@ -1,9 +1,7 @@
-#Gives active MJO events from a list of indices, ind, and minimum amplitude, min, 
-#and the length of days it occurs for each event, ocur
+#Gives active MJO events from a list of indices find the length of days it occurs for each event, ocur
 #
 #Args
-#ind      A list of indices from MJO filtered dataset 
-#min      A numeric that is of a certain value and all values above will return
+#days     A list of days
 #ocur     Amount of occurences to define an active MJO
 #
 #Output
@@ -12,4 +10,16 @@
 #Note: no error checking
 #
 
-find_ocur <- function(ind, min, ocur)
+find_ocur <- function(ind)
+
+{   
+  # transpose maindtrix and square the vector
+  
+  step1 <- c(ind)
+  step2 <- diff(unique(cumsum(step1 >=1)[step1 <= 1]))
+  step3 <- c(step2)
+  
+  # save both results in a list and return
+  final <- step3
+  return(final)
+}
