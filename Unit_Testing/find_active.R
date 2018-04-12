2982b3fb55bfdb9a49f9db757820059f02242851
Dan Reuman <reuman@ku.edu>
2018-04-12 08:25:35 -0500

    Dan comments

--- Unit_Testing/find_active.R
+++ Unit_Testing/find_active.R
@@ -1,3 +1,6 @@
+#***DAN: I did not understand until I read Tests.Rmd what this function was doing because I 
+#do not know what MJO refers to.
+
 #Takes a list of MJO indices, ind, and a minimum criteria, min, and returns a vector with 
 #the dates of all matches 
 #
@@ -15,10 +18,10 @@ find_active <- function(ind, min)
 
   {   
     # steps for function
-    step1 <- c(ind)
-    step2 <- ifelse(ind>=min, ind, 0)
+    step1 <- c(ind) #***DAN: what is the purpose of this line? seems like none
+    step2 <- ifelse(ind>=min, ind, 0) #***DAN: filling with 0s is often a bad idea unless it is impossible for MJOs (still don't know what those are) to be 0 naturally
     
     # save results and return
-    final <- step2
+    final <- step2 
     return(final)
   }
